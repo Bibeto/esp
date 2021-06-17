8480cbc1ff050f14998dab003e1190c04f2331f9
Bibeto <fersiyosri@gmail.com>
2021-06-17 15:52:28 +0100

    Added blink example

--- hello_world/main/main.c
+++ hello_world/main/main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_system.h"
 
 
 void app_main(void)
 {
     while (1) {
-        printf("Hello world!\n"); 
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
+        printf("Hello World !\n"); 
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+        fflush(stdout); 
     }
 }
